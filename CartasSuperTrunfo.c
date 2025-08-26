4cc6b8964ab6f16a157acaae8886697f34f00fbf
Lucas-Braga-Bit <lvo.braga2021@gmail.com>
2025-08-26 18:38:25 +0000

    Modificação no comentário inicial, renomeação de algumas funções para melhor entendimento, reogarnização do código, Novos comentários, Coleta do nome do estado, Algumas correções otrográficas.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
-/*Programa funcional com as funcionalidades reuquisitadas, ainda falta a adição de comentários para o melhor entendimento de terceiros.*/
+/*Programa funcional com as funcionalidades requisitadas.*/
 int main()
 {
 
-    int carta1, pop1, turismo1;
-    float pib1, area1;
-    char estado1, nomec1[20], codigo1[20];
+    int carta1, pop1, turismo1; // carta = numeração da carta, pop = população, turismo = pontos turísticos
+    float pib1, area1; // pib = PIB per capta, area = área em km²
+    char estado1, nome_C1[20], nome_E1[20]; // estado = letra que representará o estado, nome_C = nome da cidade, nome_E = nome do estado
 
-    int carta2, pop2, turismo2;
+    int carta2, pop2, turismo2; // diferenciação entre dados das cartas 1 e 2 com a adição do número à frente.
     float pib2, area2;
-    char estado2, nomec2[20], codigo2[20];
+    char estado2, nome_C2[20], nome_E2[20];
 
     printf("Olá! Este é o Desafio do Super Trunfo!\n");
-    printf("Neste programa, vamos cadastrar 2 cartas para que você possa visualizá-la nesse terminal, vamos lá?\n\n");
-
-
-    printf("Vamos começar o cadastro da primeira carta\n");
+    printf("Neste programa, vamos cadastrar 2 cartas para que você possa visualizá-la nesse terminal, vamos lá?\n\n"); // Forma de interação amigável com o usuário sendo replicada durante todo o código
+    // utilização da função printf e scanf ao longo de todo trabalho como requisitado pelo desafio.
+    // Código organizado primeiro pela coleta dos dados das cartas e depois a exibição para o usuário de todas os dados cadastrados.
     
-    printf("Digite o a letra da carta que indicará o Estado: ");
+    printf("Vamos começar o cadastro da primeira carta.\n\n"); 
+
+    printf("Digite o nome do Estado, para espaços, utilize o _ por gentiliza: "); // Começo feito pela coleta dos dados da primeira carta.
+    scanf("%s", nome_E1); 
+    
+    printf("Digite o a letra da carta que indicará o Estado, escolha uma letra de A a H: ");
     scanf(" %c", &estado1);
 
     printf("Certo! Agora digite o número da carta que indicará a cidade: ");
     scanf("%d", &carta1);
 
     printf("Certo! Agora digite o nome da cidade: ");
-    scanf("%s", nomec1);
+    scanf("%s", nome_C1);
 
     printf("Certo! Agora digite a população desta cidade: ");
     scanf("%d", &pop1);
@@ -35,20 +39,23 @@ int main()
     printf("Certo! Agora digite o PIB da cidade: ");
     scanf("%f", &pib1);
 
-    printf("Certo! Agora digite o número de pontos turísticos: ");
+    printf("Certo! Agora digite o número de pontos turísticos: \n");
     scanf("%d", &turismo1);
 
 
-    printf("Tudo pronto! Agora vamos ao cadastro da segunda carta.\n");
+    printf("Tudo pronto! Agora vamos ao cadastro da segunda carta.\n\n"); // Coleta de dados da segunda carta.
+
+    printf("Digite o nome do Estado, para espaços, utilize o _ por gentiliza: ");
+    scanf("%s", nome_E2);
     
-    printf("Digite o a letra da carta que indicará o Estado: ");
+    printf("Digite o a letra da carta que indicará o Estado, escolha uma letra de A a H: ");
     scanf(" %c", &estado2);
 
-    printf("Certo! Agora digite o número da carta que indicará a cidade: ");
+    printf("Certo! Agora digite o número da carta que indicará a cidade, escolha um Nº de 1 à 8: ");
     scanf("%d", &carta2);
 
-    printf("Certo! Agora digite o nome da cidade: ");
-    scanf("%s", nomec2);
+    printf("Certo! Agora digite o nome da cidade, siga a mesma regra do _ : ");
+    scanf("%s", nome_C2);
 
     printf("Certo! Agora digite a população desta cidade: ");
     scanf("%d", &pop2);
@@ -63,25 +70,25 @@ int main()
     scanf("%d", &turismo2);
     
 
-    printf("\n\nPronto! Agora vamos verificar as informações cadastradas:\n\n");
+    printf("\n\nPronto! Agora vamos verificar as informações cadastradas:\n\n"); // Exibição dos dados cadastrados pelo usuário.
 
     printf("Carta 1\n");
-    printf("Estado - %c\n", estado1);
+    printf("Estado - %s\n", nome_E1);
     printf("Código da carta - %c0%d\n", estado1, carta1);
-    printf("Nome da cidade - %s\n", nomec1);
+    printf("Nome da cidade - %s\n", nome_C1);
     printf("População - %d\n", pop1);
     printf("Área km² - %.3fkm²\n", area1);
     printf("PIB per Capta - %.3f\n", pib1);
     printf("Pontos Turísticos - %d\n\n", turismo1);
 
     printf("Carta 2\n");
-    printf("Estado - %c\n", estado2);
-    printf("Código da carta - %c0%d\n", estado2, carta2);
-    printf("Nome da cidade - %s\n", nomec2);
+    printf("Estado - %s\n", nome_E2);
+    printf("Código da carta - %c0%d\n", estado2, carta2); // coloquei um 0 como texto exibido pois o int não estava reconhecendo o 0, em busca de alguma ooutra forma de fazer.
+    printf("Nome da cidade - %s\n", nome_C2);
     printf("População - %d\n", pop2);
     printf("Área km² - %.3fkm²\n", area2);
     printf("PIB per Capta - %.3f\n", pib2);
-    printf("Pontos Turísticos - %d\n\n", turismo2);
+    printf("Pontos Turísticos - %d\n", turismo2);
 
     return 0;
 }
